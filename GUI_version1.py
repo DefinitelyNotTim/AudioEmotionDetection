cedb9f2b84a19dacdefed112ff0e1113dab060d7
alex636 <31320107+alex636@users.noreply.github.com>
2018-09-22 17:57:04 -0500

    Add files via upload

--- GUI_version1.py
+++ GUI_version1.py
@@ -14,18 +14,72 @@ wave_output_filename = "test.wav"
 
 #create the window for the GUI
 
+class Recording(object):
+    def __init__(self, fname, channels, rate, CHUNK):
+        self.fname= fname
+        self.channels=channels
+        self.rate=rate
+        self.CHUNK=CHUNK
+        self._p = pyaudio.PyAudio()
+        self.wavefile = self._prep_file(self.fname)
+        self._stream = None
+
+    #I'm not sure 
+
+
+    def __enter__(self):
+        return self
+    def __exit__(self, exception, value, traceback):
+        self.close()
+        
+    def startAudio(self):
+        
+
+        self._stream = self._p.open(format=pyaudio.paInt16,channels= self.channels,rate=self.rate,input=True,frames_per_buffer=self.CHUNK, stream_callback=self.get_callback())
+        print("recording...")
+
+        self._stream.start_stream()
+        return self
+    def stopAudio(self):
+        self._stream.stop_stream()
+        print("done recording")
+        return self
+    def get_callback(self):
+        def callback(in_data, frame_count, time_info, status):
+            self.wavefile.writeframes(in_data)
+            return in_data, pyaudio.paContinue
+        return callback
+
+    
+    def close(self):
+        self._stream.close()
+        self._p.terminate()
+        self.waveFile.close()
+        
+
+    def _prep_file(self, fname):
+        waveFile = wave.open(wave_output_filename,'wb')
+        waveFile.setnchannels(self.channels)
+        waveFile.setsampwidth(self._p.get_sample_size(pyaudio.paInt16))
+        waveFile.setframerate(self.rate)
+        return waveFile
+    
 
 class Application(Frame):
     def __init__(self,master):
         Frame.__init__(self,master)
         self.grid(column = 2, row = 5)
         self.create_widgets()
+        self.recorder=Recording(wave_output_filename, CHANNELS, RATE, CHUNK)
         #self.
     
 
     def create_widgets(self):
         self.startButton = Button(self, text = " Start recording    " , justify = "left", command = self.recordAudio)
         self.startButton.grid()
+
+        self.stopButton = Button(self, text = " Stop recording    " , justify = "left", command = self.endAudio)
+        self.stopButton.grid()
         
         self.playButton = Button(self, text = "Play Recorded Audio", justify = "left")
         self.playButton.grid()
@@ -44,31 +98,17 @@ class Application(Frame):
 
         self.label = Label(self, text = "User Name")
         self.label.grid(column = 1, row = 0)
-        
+
     def recordAudio(self):
-        p = pyaudio.PyAudio()
-
-        stream = p.open(format=FORMAT,channels=CHANNELS,rate=RATE,input=True,frames_per_buffer=CHUNK)
-        print("recording...")
-
-        frames = []
-
-        for i in range (0, int(RATE/CHUNK*record_seconds)):
-            data = stream.read(CHUNK)
-            frames.append(data)
-
-        print("done recording")
+        self.recorder.startAudio()
+        return self
+    def endAudio(self):
+        self.recorder.stopAudio()
+        return self
         
-        stream.stop_stream()
-        stream.close()
-        p.terminate()
         
-        waveFile = wave.open(wave_output_filename,'wb')
-        waveFile.setnchannels(CHANNELS)
-        waveFile.setsampwidth(p.get_sample_size(FORMAT))
-        waveFile.setframerate(RATE)
-        waveFile.writeframes(b''.join(frames))
-        waveFile.close()
+    
+
 
 #modify root window
 root = Tk()
