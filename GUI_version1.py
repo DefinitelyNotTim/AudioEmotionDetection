55d1369414a32a135e48e7192db1ff58744aba29
Humberto Colin <colin010@live.missouristate.edu>
2018-09-21 19:14:28 -0500

    added basic recording for 10 seconds unable to stop manually. working on that. stores file automatically as wavefile in the current folder. the current recording is also always replaced if you try again.

--- GUI_version1.py
+++ GUI_version1.py
@@ -1,6 +1,16 @@
 #Gui interface for Audio Emotional detection program CSC-450 Michael Knapp
 
 from tkinter import *
+import pyaudio
+import wave
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+record_seconds = 10
+wave_output_filename = "test.wav"
+
 
 #create the window for the GUI
 
@@ -14,21 +24,20 @@ class Application(Frame):
     
 
     def create_widgets(self):
-        self.button1 = Button(self, text = " Start recording    " , justify = "left",)
-        self.button1.grid()
+        self.startButton = Button(self, text = " Start recording    " , justify = "left", command = self.recordAudio)
+        self.startButton.grid()
         
+        self.playButton = Button(self, text = "Play Recorded Audio", justify = "left")
+        self.playButton.grid()
 
-        self.button2 = Button(self, text = "Play Recorded Audio", justify = "left")
-        self.button2.grid()
+        self.saveButton = Button (self, text = "Save Audio    ", justify = "left")
+        self.saveButton.grid()
 
-        self.button3 = Button (self, text = "Save Audio    ", justify = "left")
-        self.button3.grid()
+        self.deleteButton = Button (self,text = "Delete Audio   ", justify = "left")
+        self.deleteButton.grid()
 
-        self.button4 = Button (self,text = "Delete Audio   ", justify = "left")
-        self.button4.grid()
-
-        self.button5 = Button(self,text = "Process Emotion  ", justify = "left")
-        self.button5.grid()
+        self.processButton = Button(self,text = "Process Emotion  ", justify = "left")
+        self.processButton.grid()
 
         self.txt = Entry(self, width = 20)
         self.txt.grid(column = 2, row = 0)
@@ -36,7 +45,30 @@ class Application(Frame):
         self.label = Label(self, text = "User Name")
         self.label.grid(column = 1, row = 0)
         
+    def recordAudio(self):
+        p = pyaudio.PyAudio()
+
+        stream = p.open(format=FORMAT,channels=CHANNELS,rate=RATE,input=True,frames_per_buffer=CHUNK)
+        print("recording...")
+
+        frames = []
+
+        for i in range (0, int(RATE/CHUNK*record_seconds)):
+            data = stream.read(CHUNK)
+            frames.append(data)
+
+        print("done recording")
         
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+        
+        waveFile = wave.open(wave_output_filename,'wb')
+        waveFile.setnchannels(CHANNELS)
+        waveFile.setsampwidth(p.get_sample_size(FORMAT))
+        waveFile.setframerate(RATE)
+        waveFile.writeframes(b''.join(frames))
+        waveFile.close()
 
 #modify root window
 root = Tk()
