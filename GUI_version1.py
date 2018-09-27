361015787aa7eb326d75ad08568fc951d4d7c0b8
alex636 <31320107+alex636@users.noreply.github.com>
2018-09-27 09:17:14 -0500

    Add files via upload

--- GUI_version1.py
+++ GUI_version1.py
@@ -2,76 +2,26 @@
 
 from tkinter import *
 import pyaudio
-import wave
+import Recording
+import emotionProcessor
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
-CHANNELS = 2
+CHANNELS = 1
 RATE = 44100
-record_seconds = 10
 wave_output_filename = "test.wav"
 
 
 #create the window for the GUI
 
-class Recording(object):
-    def __init__(self, fname, channels, rate, CHUNK):
-        self.fname= fname
-        self.channels=channels
-        self.rate=rate
-        self.CHUNK=CHUNK
-        self._p = pyaudio.PyAudio()
-        self.wavefile = self._prep_file(self.fname)
-        self._stream = None
-
-    #I'm not sure sure what these next two functions are for unless they have something to do with creating and destroying the object, they never seem to run. However, when I was looking into how to do this callback online this is how they recommended that I organize the code
-
-
-    def __enter__(self):
-        return self
-    def __exit__(self, exception, value, traceback):
-        self.close()
-        
-    def startAudio(self):
-        
-
-        self._stream = self._p.open(format=pyaudio.paInt16,channels= self.channels,rate=self.rate,input=True,frames_per_buffer=self.CHUNK, stream_callback=self.get_callback())
-        print("recording...")
-
-        self._stream.start_stream()
-        return self
-    def stopAudio(self):
-        self._stream.stop_stream()
-        print("done recording")
-        return self
-    def get_callback(self):
-        def callback(in_data, frame_count, time_info, status):
-            self.wavefile.writeframes(in_data)
-            return in_data, pyaudio.paContinue
-        return callback
-
-    
-    def close(self):
-        self._stream.close()
-        self._p.terminate()
-        self.waveFile.close()
-        
-
-    def _prep_file(self, fname):
-        waveFile = wave.open(wave_output_filename,'wb')
-        waveFile.setnchannels(self.channels)
-        waveFile.setsampwidth(self._p.get_sample_size(pyaudio.paInt16))
-        waveFile.setframerate(self.rate)
-        return waveFile
-    
 
 class Application(Frame):
     def __init__(self,master):
         Frame.__init__(self,master)
         self.grid(column = 2, row = 5)
         self.create_widgets()
-        self.recorder=Recording(wave_output_filename, CHANNELS, RATE, CHUNK)
-        #self.
+        self.recorder=Recording.Recording(wave_output_filename, CHANNELS, RATE, CHUNK)
+        self.processor=emotionProcessor.EmotionProcessor(wave_output_filename)
     
 
     def create_widgets(self):
@@ -81,16 +31,16 @@ class Application(Frame):
         self.stopButton = Button(self, text = " Stop recording    " , justify = "left", command = self.endAudio)
         self.stopButton.grid()
         
-        self.playButton = Button(self, text = "Play Recorded Audio", justify = "left")
+        self.playButton = Button(self, text = "Play Recorded Audio", justify = "left", command = self.playAudio)
         self.playButton.grid()
 
-        self.saveButton = Button (self, text = "Save Audio    ", justify = "left")
+        self.saveButton = Button (self, text = "Save Audio    ", justify = "left", command = self.saveAudio)
         self.saveButton.grid()
 
-        self.deleteButton = Button (self,text = "Delete Audio   ", justify = "left")
+        self.deleteButton = Button (self,text = "Delete Audio   ", justify = "left", command = self.deleteAudio)
         self.deleteButton.grid()
 
-        self.processButton = Button(self,text = "Process Emotion  ", justify = "left")
+        self.processButton = Button(self,text = "Process Emotion  ", justify = "left", command = self.processAudio)
         self.processButton.grid()
 
         self.txt = Entry(self, width = 20)
@@ -98,6 +48,18 @@ class Application(Frame):
 
         self.label = Label(self, text = "User Name")
         self.label.grid(column = 1, row = 0)
+        
+        self.label = Label(self, text = "User Name:")
+        self.label.grid(column = 2, row = 0)
+
+        self.text = Entry(self, width = 20)
+        self.text.grid(column = 3, row = 0)
+        
+        self.label = Label (self, text = "Emotion:")
+        self.label.grid(column = 2, row = 1)
+
+        self.text = Entry(self, width = 20)
+        self.text.grid(column = 3, row = 1)
 
     def recordAudio(self):
         self.recorder.startAudio()
@@ -105,7 +67,15 @@ class Application(Frame):
     def endAudio(self):
         self.recorder.stopAudio()
         return self
-        
+    def processAudio(self):
+        self.processor.pitchProc()
+        return self
+    def playAudio(self):
+        return self
+    def saveAudio(self):
+        return self
+    def deleteAudio(self):
+        return self
         
     
 
