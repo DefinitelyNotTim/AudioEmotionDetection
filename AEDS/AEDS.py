c5b3baa7ace609599e061082e6cfd11ffe8075b5
Timmothy Lane <34077530+DefinitelyNotTim@users.noreply.github.com>
2018-11-25 21:23:22 -0600

    Add files via upload

---
+++ AEDS/AEDS.py
@@ -0,0 +1,120 @@
+from tkinter import *
+import tkinter as tk
+import pyaudio
+import Recording
+import emotionProcessor
+from tkinter import Menu
+from tkinter import messagebox as mbox
+import scikit_network
+from statistics import stdev
+import numpy as np
+from profileManager import *
+
+# Hardcoded Variables 
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+wave_output_filename = "test.wav"
+
+
+# Code to create the window for the GUI.
+# this class sets up the frame which is the entire window needed to fit the GUI buttons and lable's inside.
+
+class Application(Frame):
+    def __init__(self,master):
+        Frame.__init__(self,master)
+        self.grid(column = 1, row = 5)
+               
+        self.create_widgets()
+        self.recorder=Recording.Recording(wave_output_filename, CHANNELS, RATE, CHUNK)
+        self.processor=emotionProcessor.EmotionProcessor(wave_output_filename)
+
+        
+
+    
+   # class widgets.. this is where the code for each off six buttons two lable's and two text boxes are held.
+   # Grid not pack is used.
+   # grid= where is that button on lable located in the frame.
+   # each button has a command atribute that connects the button with a function that controls what the button does.
+   
+    def create_widgets(self):
+       
+        #Start button is added. 
+       
+        self.startButton = Button(self, text = " Start: recording          " , justify = "center", command = self.recordAudio, bg = "lightgray")
+        self.startButton.grid(row = 0, column = 0)
+        
+        # Stop button is added.
+        
+        self.stopButton = Button(self, text = " Stop: recording          " , justify = "center", command = self.endAudio, bg ="lightgray")
+        self.stopButton.grid(row = 1, column = 0)
+        
+        
+        # Label for the output below "user Id or name".
+            
+        self.label = Label(self, text = "User Name:               ", font = 13, justify = "left")
+        self.label.grid(column = 1, row = 0)
+    
+        #output for the User id that is either chosen after the emotion is processed or just chosen still an option we are deciding on.
+        self.user = StringVar()
+        self.txt = Entry(self, textvariable = self.user, width = 32)
+        self.txt.grid(column = 1, row = 1)
+            
+        # Label for the GUI that Says " predicted emotion".        
+        self.label = Label (self, text = "Predicted Emotion:    ",font = 13, justify = "left")
+        self.label.grid(column = 1, row = 2)
+        
+        # Output field for the label where the predicted emotion will be added when the NN processed the audio matrics hopefully correctly. 
+        self.emotionalPrediction = StringVar()
+        self.text = Entry(self, textvariable = self.emotionalPrediction, width = 32)
+        self.text.grid(column = 1, row = 3)
+
+        
+        
+    def recordAudio(self):
+        self.recorder=Recording.Recording(wave_output_filename, CHANNELS, RATE, CHUNK)
+        self.recorder.startAudio()
+        self.emotionalPrediction.set("Recording...")
+        return self
+    # End audio also needs a popup button.
+    
+    def endAudio(self):
+        #Stop recording audio
+        self.recorder.stopAudio()
+        #Set the box containing the emotional prediction to be blank
+        self.emotionalPrediction.set(" ")
+        #Get the entered user name from the entry box
+        self.userName = self.user.get()
+        
+        #print("USER NAME: " + self.userName)   #Debugging print
+        
+        # Call the method to get the audio metrics
+        audio_metrics = self.processor.collectMetrics()
+        # Create a user profile object using the entered user name
+        user_profile = profileManager(self.userName)
+        # Access the profile for the given user
+        user_profile.accessProfile()
+        #predicted = scikit_network.compare_new(audio_metrics)
+        predicted = scikit_network.compare_new(audio_metrics, user_profile)
+        self.emotionalPrediction.set(predicted[0])
+        #user_profile.writeToProfile(audio_metrics, "nervous")
+        return self
+    
+
+
+# Modify root window.
+root = Tk()
+root.title("Audio Control Interface")
+
+# The size of the whole frame.
+
+root.geometry ("300x158") 
+
+# Background for the whole GUI
+
+app = Application(root)
+
+#kick off the event loop
+root.mainloop()
+
---
+++ AEDS/profileManager.py
@@ -0,0 +1,89 @@
+## profileManager.py: Contains the profileManager class that allows access
+##  and manipulations of user profiles for the Audio Emotion Detection System
+##  Contains the following methods:
+##      __init(self, userName)__: Initialization method
+##      accessProfile(self): Accesses uer profiles
+##      writeToProfile(self, newMetrics, Emotion): Updates user profiles
+##      generateProfile(self): Creates new user profiles
+##  More information regarding these methods are detailed above each method.
+##  Unless otherwise specified, every method within this class was written by Timmothy Lane
+
+from pathlib import Path
+import os.path
+from pandas import *
+
+
+class profileManager:
+
+
+## Initalization class for the profile manager, which is called every time 
+##  a new profile object is created.
+##  This method sets the file name and file path associated with a user
+##  profile.
+    def __init__(self, userName):
+        #If receieved userName is empty or null
+        if userName == "" or userName == None:
+            #set the file path and name to match the generic file
+            self.fileName = "generic.csv"
+            self.path = "profiles/"+self.fileName
+        #If a userName is provided
+        else:
+            #Set the file name and path to match the user name
+            self.fileName = userName +".csv"
+            self.path = "profiles/"+self.fileName
+
+
+
+## Method that accesses a user profile .csv file.
+##  Checks to see if a user profile .csv exists, and
+##   calls the generateProfile method if one does not.
+##  Creates a dataframe from the user profile so that
+##   the metrics can be easier accessed.
+    def accessProfile(self):
+        # Use the path attribute to create a Path for the file
+        self.file = Path(self.path)
+        #print(self.file)       #print statement for debugging
+        # If the file associated with the user  profile exists
+        if self.file.exists():
+            # Create a dataframe holding the information of the user profile using pandas
+            self.oldMetrics = pandas.read_csv(self.path, header = None, sep = ',', names = ['Pitch', 'Tone', 'SPL', 'wordGap' , 'WordGapLen', 'Emotion'])
+        # If a file does not exist for the user profile
+        else:
+            # Generate a new file for the user profile
+            self.generateProfile()
+            # Create a dataframe holding the information of the user profile using pandas
+            self.oldMetrics = pandas.read_csv(self.path, header = None, sep = ',', names = ['Pitch', 'Tone', 'SPL', 'wordGap' , 'WordGapLen', 'Emotion'])
+
+
+
+##  Method that allows writing to the .csv associated with the user.
+##  Inputs: the new audio metrics and the corrosponding emotion
+##  Outputs: None
+##  The method updates the row of the user profile with the corrosponding emotion
+##      with the new metrics
+    def writeToProfile(self, newMetrics, emotion):
+        self.accessProfile()
+        #print(self.oldMetrics.loc[self.oldMetrics['Emotion'] == emotion])
+        self.oldMetrics.loc[self.oldMetrics['Emotion'] == emotion, "Pitch"] = newMetrics[0]
+        self.oldMetrics.loc[self.oldMetrics['Emotion'] == emotion, "Tone"] = newMetrics[1]
+        self.oldMetrics.loc[self.oldMetrics['Emotion'] == emotion, "SPL"] = newMetrics[2]
+        self.oldMetrics.loc[self.oldMetrics['Emotion'] == emotion, "wordGap"] = newMetrics[3]
+        self.oldMetrics.loc[self.oldMetrics['Emotion'] == emotion, "WordGapLen"] = newMetrics[4]
+        #print(self.oldMetrics.loc[self.oldMetrics['Emotion'] == emotion])
+        self.oldMetrics.to_csv(self.path, index = False, header = 0)
+        return self.path
+
+## Method for genereating a new .csv file for a new user.
+##  Creates a new .csv and sets it's values to the same values
+##  found in the generic user profile.
+    def generateProfile(self):
+        # Create a dataframe holding the information of the generic profile using pandas
+        generic = pandas.read_csv("profiles/generic.csv", header = None, sep = ',', names = ['Pitch', 'Tone', 'SPL', 'wordGap' , 'WordGapLen', 'Emotion'])
+        # Write the dataframe to a new .csv file that will be associated with the
+        #   current user profile
+        generic.to_csv(self.path, index = False, header = 0)
+        return self.path
+
+    
+
+
