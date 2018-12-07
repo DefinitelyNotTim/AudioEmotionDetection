5b0fccb35af3150c3744c24a5120c5a6c596f9b6
Timmothy Lane <34077530+DefinitelyNotTim@users.noreply.github.com>
2018-12-07 15:45:52 -0600

    Add files via upload

--- AEDS/scikit_network.py
+++ AEDS/scikit_network.py
@@ -1,11 +1,24 @@
+##  scikit_network.py:
+##      Allows the AEDS software to make predictions about the user's
+##      emotional state by comparing new vocal metrics against previously
+##      stored metrics using the k-nearest neighbor algorithm.
+##  Related Software Requirements: FR.8, FR.9
+##  Authors: Bryan Jones and Mark Webb
 import numpy as np
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from profileManager import *
 
 
+## compare_new(): Compares data pertaining to the new recording and compares
+##  it against the data retrieved from the user profile using the k-nearest
+##  neighbor algorithm to find the closest match for the new data.
+## Inputs: Data conveying the vocal metrics from the recent audio recording,
+##          and the user profile.
+## Outputs: The predicted emotional state of the user.
+## Authors: Bryan Jones and Mark Webb
+## Related Software Requirements:
 def compare_new(new_metrics, user_profile):
-    #emotion_data = "data.csv"
     # Changed the emotion data to use user profile data
     # Tim - 11/24
     emotion_data = user_profile.path
